fbdc6f53392d8b3d7735ed468a8a57a5c9ced8f0
BHARATH2145 <94664858+Bharath2145@users.noreply.github.com>
2022-03-04 09:48:31 +0000

    debugged p1

--- p1final.c
+++ p1final.c
@@ -1,1 +1,42 @@
-#include <stdio.h>
+  #include <stdio.h>
+void input(int *num1 ,int *den1,int *num2,int *den2)
+{
+	printf("enter the numerator and denomenator of fraction\n");
+	scanf("%d %d %d %d",num1,den1,num2,den2);
+}
+void add(int num1,int den1,int num2,int den2,int *num3,int *den3)
+{
+	
+	  if(den1%den2==0)
+		{
+			*den3 = den1 ;
+			*num3 = num1 + (num2)*(den1/den2);
+		}
+	else if(den2%den1==0)
+		{
+			*den3 = den2 ;
+			*num3 = num2 + (num1)*(den2/den1);
+		}
+		else if(den1 = den2)
+		{
+			*den3 = den1;
+			*num3 = num1 + num2;
+		}
+	else
+	{
+			*den3 = (den1)*(den2);
+			*num3 = (num1)*(den2) + (num2)*(den1) ;
+	}
+}
+void output(int num1,int den1,int num2,int den2,int num3,int den3)
+{
+	printf("%d/%d + %d/%d is %d/%d \n",num1,den1,num2,den2,num3,den3);
+}
+int main()
+{
+	int num1,num2,num3,den1,den2,den3;
+	input(&num1,&den1,&num2,&den2);
+	add(num1,den1,num2,den2,&num3,&den3);
+	output(num1,den1,num2,den2,num3,den3);
+	return 0;
+}
